ab2c2e69e41fd256b9c6cc27c992ab29e872d775
chuckshunkPF <chuck.shunk@powerfactorscorp.com>
2018-03-09 11:12:03 -0500

    f3

--- samplePython.py
+++ samplePython.py
@@ -8,7 +8,7 @@ number = random.randint(1, 20)
 print 'Well, {0}, I am thinking of a number between 1 and 20.'.format(name)
 
 while guesses_made < 6:
-    print '4'
+    print '7'
 
     guess = int(raw_input('Take a guess: '))
 
